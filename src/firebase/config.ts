d2469d6ed335c32c4710aa56eceb49a522be58de
Mats Vink <matsvink05@gmail.com>
2025-12-24 11:55:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3646492903-a8620",
+  "appId": "1:1063418464090:web:8b3dae3af5e422fea1d563",
+  "apiKey": "AIzaSyDSjHa3LPZiznGYezrguvAOYNcz0NrzKI8",
+  "authDomain": "studio-3646492903-a8620.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1063418464090"
+};
