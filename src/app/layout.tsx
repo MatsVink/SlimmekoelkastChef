565cfd277c2e025e703baed27a5da97e238b18b6
Mats Vink <matsvink05@gmail.com>
2025-12-24 13:49:11 +0000

    ik krijg dit in het f12 cosole: VM105:13 Executing inline script violate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,12 +17,6 @@ export default function RootLayout({
   return (
     <html lang="nl" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <FirebaseClientProvider>
